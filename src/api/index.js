57bc75ec0f80adbdb23bd1b28986999b073cbec1
Ashish <sainiashish356@gmail.com>
2022-06-16 20:20:51 +0530

    added custom fetch function

--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,54 @@
-const customFetch = () => {};
+import { LOCALSTORAGE_TOKEN_KEY } from "../utils";
+
+const customFetch = async (url , {body , ...customConfig }) => {
+         //got the token
+        const token = window.localStorage.getItem(LOCALSTORAGE_TOKEN_KEY);
+
+        
+
+        //setting headers
+        const headers = {
+            //setting the content type
+            'content-type': 'application/json',
+            //accepting applicaiton json we're dealing with json means we're sendind nd recieving json
+            Accept: 'application/json'
+        }
+
+        if (token) {
+            headers.Authorization = `Bearer ${token}`
+        }
+
+        const config = {
+            ...custommConfig,
+            header: {
+                ...headers,
+                ...customConfig.headers,
+            },
+        };
+            //if there is a body in second arguement we stringify
+        if (body){
+            config.body = JSON.stringify(body);
+        }
+
+    try{
+        const response = await fetch(url,config);
+        const data = await response.JSON();
+        
+        if (response.success){
+            return{
+                data: data.data,
+                success: true
+            }
+        }
+            throw new Error(data.message);
+    }catch(error){
+        console.log("error");
+        return{
+            message: error.message,
+            success: false
+        }
+    }
+};
 
 const getPosts = (page,limit) => {
     return customFetch();
