6e7ce455dd9788139104700458516f93425031e0
Ashish <sainiashish356@gmail.com>
2022-06-17 17:27:54 +0530

    Created UI of Home Page with CSS Module

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,56 +1,58 @@
- const Home = () => {
-    return (
-        <div className="posts-list">
-        <div className="post-wrapper">
-          <div className="post-header">
-            <div className="post-avatar">
+import styles from '../styles/home.module.css';
+
+const Home = () => {
+  return (
+    <div className={styles.postsList}>
+      <div className={styles.postWrapper}>
+        <div className={styles.postHeader}>
+          <div className={styles.postAvatar}>
+            <img
+              src="https://cdn-icons.flaticon.com/png/512/2202/premium/2202112.png?token=exp=1655467517~hmac=bd0f149d2e160f8cedacb8f20242b7c7"
+              alt="user-pic"
+            />
+            <div>
+              <span className={styles.postAuthor}>Ashish Saini</span>
+              <span className={styles.postTime}>a minute ago</span>
+            </div>
+          </div>
+          <div className={styles.postContent}>Post Content</div>
+
+          <div className={styles.postActions}>
+            <div className={styles.postLike}>
               <img
-                src="https://cdn-icons.flaticon.com/png/512/2202/premium/2202112.png?token=exp=1655420809~hmac=7f21abb7bab5be3f0e063e7afba63797"
-                alt="user-pic"
+                src="https://cdn-icons-png.flaticon.com/512/889/889140.png"
+                alt="likes-icon"
               />
-              <div>
-                <span className="post-author">Ashish</span>
-                <span className="post-time">a minute ago</span>
-              </div>
+              <span>5</span>
             </div>
-            <div className="post-content">Post Content</div>
-  
-            <div className="post-actions">
-              <div className="post-like">
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/686/686308.png"
-                  alt="likes-icon"
-                />
-                <span>5</span>
-              </div>
-  
-              <div className="post-comments-icon">
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/1330/1330233.png"
-                  alt="comments-icon"
-                />
-                <span>2</span>
-              </div>
+
+            <div className={styles.postCommentsIcon}>
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/1329/1329927.png"
+                alt="comments-icon"
+              />
+              <span>2</span>
             </div>
-            <div className="post-comment-box">
-              <input placeholder="Start typing a comment" />
-            </div>
-  
-            <div className="post-comments-list">
-              <div className="post-comments-item">
-                <div className="post-comment-header">
-                  <span className="post-comment-author">Bill</span>
-                  <span className="post-comment-time">a minute ago</span>
-                  <span className="post-comment-likes">22</span>
-                </div>
-  
-                <div className="post-comment-content">Random comment</div>
+          </div>
+          <div className={styles.postCommentBox}>
+            <input placeholder="Start typing a comment" />
+          </div>
+
+          <div className={styles.postCommentsList}>
+            <div className={styles.postCommentsItem}>
+              <div className={styles.postCommentHeader}>
+                <span className={styles.postCommentAuthor}>Bill</span>
+                <span className={styles.postCommentTime}>a minute ago</span>
+                <span className={styles.postCommentLikes}>22</span>
               </div>
+
+              <div className={styles.postCommentContent}>Random comment</div>
             </div>
           </div>
         </div>
       </div>
-    )
+    </div>
+  );
 };
 
 export default Home;
