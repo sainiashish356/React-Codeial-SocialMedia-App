3034da7cb1b24d4d679443c7167a3fb5d8b5f8dd
Ashish <sainiashish356@gmail.com>
2022-06-24 06:12:36 +0530

    user login API call implemented

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,52 +1,69 @@
-import React , {useState} from 'react';
-import styles from '../styles/login.module.css';
+import { useState } from 'react';
 import { useToasts } from 'react-toast-notifications';
 
+import styles from '../styles/login.module.css';
+import { login } from '../api';
+
 const Login = () => {
-    const [email , setEmail] = useState('');
-    const [password , setPassword] = useState('');
-    const [loggingIn , setLoggingIn] = useState(false);
-    const { addToast } = useToasts();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loggingIn, setLoggingIn] = useState(false);
+  const { addToast } = useToasts();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoggingIn(true);
 
-        setLoggingIn(true);
-
-        if(!email || !password){
-            return addToast('Please enter both email and Password' , {
-                // these are the optional statements
-                appearance: 'error',
-                autoDismiss: true,
-            })
-        }
-
+    if (!email || !password) {
+      return addToast('Please enter both email and password', {
+        appearance: 'error',
+      });
     }
 
-    return <form className={styles.loginForm} onSubmit={handleSubmit}>
-        <span className={styles.loginSignupHeader}>Login</span>
+    const response = await login(email, password);
 
-        <div className={styles.field}>
-            <input 
-            type="email"
-             placeholder="Email"
-              value={email}
-               onChange = { (e) => setEmail(e.target.value) }/>
-        </div>
+    if (response.success) {
+      addToast('Successfully logged in', {
+        appearance: 'success',
+      });
+    } else {
+      addToast(response.message, {
+        appearance: 'error',
+      });
+    }
 
-        <div className={styles.field}>
-            <input type="password"
-             placeholder="Password"
-                value={password}
-                onChange = { (e) => setPassword(e.target.value) }
-              />
-        </div>
+    setLoggingIn(false);
+  };
 
-        <div className={styles.field} disabled={loggingIn}>
-            <button> {loggingIn ? 'Logging in..' : 'Log in'} </button>
-        </div>
+  return (
+    <form className={styles.loginForm} onSubmit={handleSubmit}>
+      <span className={styles.loginSignupHeader}>Log In</span>
 
+      <div className={styles.field}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+
+      <div className={styles.field}>
+        <input
+          type="password"
+          placeholder="Paasword"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+
+      <div className={styles.field}>
+        <button disabled={loggingIn}>
+          {loggingIn ? 'Logging in...' : 'Log In'}
+        </button>
+      </div>
     </form>
+  );
 };
 
 export default Login;
